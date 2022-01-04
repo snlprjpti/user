9878ed50e0922e4ed94e7aaf9f7e50ea7924f299
Sunil Prajapati <sunil.prajapati@hazesoft.co>
2022-01-04 16:01:00 +0545

    updated

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '9759303e3e2644f8779538766b28a014cf07aa67',
+        'name' => 'snlprjti/user',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'snlprjti/user' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '9759303e3e2644f8779538766b28a014cf07aa67',
+            'dev_requirement' => false,
+        ),
+    ),
+);
